53f43709beb37266a1fbc7429e4d5e47a450e22e
KyleDixonVO <KyleDixon@hotmail.ca>
2021-09-20 12:42:06 -0300

    Update 1
    Writing more code

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -11,7 +11,24 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Howdy!");
+
+            //int = integer
+            int score; //creates integer named score
+            score = 0; //set the value of score, in this case 0
+
+            Console.WriteLine(score); //accesses value of score
+            Console.WriteLine("score"); //does not access value, writes quoted characters
+
+            score = 100; //assigns score a new value, +100 points
+
+            Console.WriteLine(score);
+            Console.ReadKey(true); //waits for keypress
+
+            score = score + 100; //adds 100 to the value to score
+
+            Console.WriteLine(score);
             Console.ReadKey(true);
+
         }
     }
 }
